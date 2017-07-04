5ff4edb78617aea4562ed402089de6d52604ee83
Aditya Srinivasan <asrinivasan@ucla.edu>
2017-07-04 12:51:27 -0700

    Code refactoring; output is now more readable

--- stock-share-calc.py
+++ stock-share-calc.py
@@ -7,98 +7,97 @@ from googlefinance import getQuotes
 # 10^12
 UPPER_LIMIT = 1000000000000
 
-LAST_TRADE_STR = "LastTradePrice"
+LAST_TRADE_STR = "LastTradePrice" # from googlefinance API
 
-def init():
-    print("Please enter your budget in USD.")
-    val = sys.stdin.readline().strip('\n')
-    budget = round(float(val), 2)
+class Stock:
+    def __init__(self, stock_ticker, stock_price, stock_percent, budget):
+        self.ticker = stock_ticker
+        self.price = stock_price
+        self.percent = stock_percent/100
+        self.expected_investment = budget * self.percent
 
-    stock_cost = []
-    stock_percentages = []
-    expected_cost = []
+class InvestmentInfo:
+    def __init__(self):
+        print("*** Please enter your budget in USD. ***")
+        val = sys.stdin.readline().strip('\n')
 
-    percent_left = 100
-    while percent_left != 0:
-        print("Please enter the ticker symbol of one of the stocks in this investment.")
-        stock_ticker = sys.stdin.readline().strip('\n')
+        self.budget = round(float(val), 2)
+        self.stocks = []
 
-        stock_price = getQuotes(stock_ticker)[0][LAST_TRADE_STR]
-        print("The current price of " + stock_ticker + " is: " + stock_price)
-        print("Enter a percentage of the investment to allocate to this stock. Percent left: " + str(percent_left))
+        percent_left = 100
+        while percent_left != 0:
+            print("*** Please enter a ticker symbol. ***")
+            stock_ticker = sys.stdin.readline().strip('\n')
 
-        stock_percent = round(float(sys.stdin.readline()), 2)
-        while stock_percent > percent_left:
-            print("Invalid percentage. Please enter a percentage less than or equal to " + str(percent_left) + " for this stock.")
+            stock_price = round(float(getQuotes(stock_ticker)[0][LAST_TRADE_STR]), 2)
+            print("The current price of " + stock_ticker + " is: " + str(stock_price))
+            print("*** Enter a percentage of the investment to allocate to this stock. Percent left: " + str(percent_left) + " ***")
+
             stock_percent = round(float(sys.stdin.readline()), 2)
+            while stock_percent > percent_left:
+                print("*** Invalid percentage. Please enter a percentage less than or equal to " + str(percent_left) + " for this stock. ***")
+                stock_percent = round(float(sys.stdin.readline()), 2)
 
-        percent_left = round(percent_left - stock_percent, 2)
-        stock_cost.append(float(stock_price))
-        stock_percentages.append(stock_percent/100)
-        expected_cost.append(budget * stock_percent/100)
+            percent_left = round(percent_left - stock_percent, 2)
+            self.stocks.append(Stock(stock_ticker, stock_price, stock_percent, self.budget))
 
-    print("---------------------------------------")
-    return [budget, stock_cost, stock_percentages, expected_cost]
+        print("---------------------------------------")
 
 
-def cost_function(budget, expected_cost, current_shares, stock_cost):
+def cost_function(investment_info, current_shares):
     total_cost = 0.0
-
-    n = len(stock_cost)
+    n = len(investment_info.stocks)
 
     for i in range(n):
-        total_cost += current_shares[i] * stock_cost[i]
+        total_cost += current_shares[i] * investment_info.stocks[i].price
 
-    if budget < total_cost:
+    if investment_info.budget < total_cost:
         return UPPER_LIMIT
     else:
-        total_cost = budget - total_cost
-
+        total_cost = investment_info.budget - total_cost
         for i in range(n):
-            total_cost += math.fabs(expected_cost[i] - current_shares[i] * stock_cost[i])
+            stock = investment_info.stocks[i]
+            total_cost += math.fabs(stock.expected_investment - current_shares[i] * stock.price)
 
     return total_cost
 
 
 def main():
-    inputs = init()
-
-    budget = inputs[0]
-    stock_cost = inputs[1]
-    stock_percent = inputs[2]
-    expected_cost = inputs[3]
-
-    n = len(stock_percent)
+    investment_info = InvestmentInfo()
+    n = len(investment_info.stocks)
     shares = [0] * n
 
     for i in range(n):
-        while (shares[i] + 1) * stock_cost[i] <= expected_cost[i]:
+        stock = investment_info.stocks[i]
+        while (shares[i] + 1) * stock.price <= stock.expected_investment:
             shares[i] += 1
 
     index = 0
-
     while index != -1:
         index = -1
         min_cost = UPPER_LIMIT
 
         for i in range(n):
             shares[i] += 1
-            cur_cost = cost_function(budget, expected_cost, shares, stock_cost)
-            if cur_cost < min_cost:
-                min_cost = cur_cost
+            curr_cost = cost_function(investment_info, shares)
+            if curr_cost < min_cost:
+                min_cost = curr_cost
                 index = i
+
             shares[i] -= 1
 
         if index != -1:
             shares[index] += 1
 
-    total_investment = 0.0
+    final_investment = 0.0
+    print("*** INVESTMENT BREAKDOWN ***")
     for i in range(n):
-        print(str(shares[i]) + " ")
-        total_investment += shares[i] * stock_cost[i]
+        stock = investment_info.stocks[i]
+        print(stock.ticker + " --- " + str(shares[i]) + " shares")
+        final_investment += shares[i] * stock.price
 
-    print("Budget: " + str(budget))
-    print("Investment: " + str(total_investment))
+    print("Budget: $" + str(investment_info.budget))
+    print("Investment: $" + str(final_investment))
 
 
 if __name__ == "__main__":
